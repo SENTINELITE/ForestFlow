8c5bd07516dea726d7ec8ff186922744f34f23f3
Roman Indermühle <roman.indermuehle@icloud.com>
2024-01-05 17:12:43 +0100

    added WoodTypeAddView

---
+++ ForestFlow/App/WoodType/WoodTypeAddView.swift
@@ -0,0 +1,51 @@
+//
+//  WoodTypeAddView.swift
+//  ForestFlow
+//
+//  Created by Roman Indermühle on 05.01.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct WoodTypeAddView: View {
+    @Query private var woodTypes: [WoodType]
+    @Environment(\.modelContext) var context
+    
+    @State private var showAlert = false
+    
+    @State private var name: String = ""
+    
+    var body: some View {
+        List {
+            ForEach(woodTypes, id: \.self) { woodType in
+                Text(woodType.name)
+            }
+        }
+        .navigationTitle("Baumart")
+        .overlay(alignment: .bottomTrailing) {
+            PlusButton()
+                .button {
+                    showAlert.toggle()
+                }
+                .alert("Hinzufügen", isPresented: $showAlert, actions: {
+                    TextField("Name", text: $name)
+                    
+                   Button("Speichern", action: {
+                       saveWoodType()
+                       name = ""
+                   })
+                    Button("Abbrechen", role: .cancel, action: {})
+                }, message: {
+                    Text("Bitte gib den Namen der Baumart ein.")
+                })
+        }
+
+    }
+    
+    func saveWoodType() {
+        let woodType = WoodType(name: name)
+        context.insert(woodType)
+    }
+}
+
