5f7eb3ce574d576ae51522ff0c1949786d124fe2
Roman Indermühle <roman.indermuehle@icloud.com>
2024-01-05 15:13:35 +0100

    updated card

--- ForestFlow/Utils/UIComponents/Card.swift
+++ ForestFlow/Utils/UIComponents/Card.swift
@@ -15,14 +15,23 @@ struct Card: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .frame(width: cardSize)
+                .frame(width: cardSize, height: 150)
+                .foregroundStyle(.white)
+                .shadow(radius: 10)
+                
             
             VStack {
                 Image(systemName: systemImage)
                     .symbolRenderingMode(.hierarchical)
                     .foregroundStyle(Color.accentColor)
+                    .font(.Bold.large)
+                    .frame(width: cardSize, alignment: .topLeading)
+                    .offset(x: 15, y: -20)
+                
+                Text(name)
+                    .font(.Bold.title4)
             }
-            Text(name)
+           
         }
     }
 }
