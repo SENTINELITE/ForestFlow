b0a21cd8aa42d4ff36da091c9a20fbbfb7ad6df9
Roman Indermühle <roman.indermuehle@icloud.com>
2024-01-05 16:02:23 +0100

    added ForestOwnerAddView

---
+++ ForestFlow/App/ForestOwner/ForestOwnerAddView.swift
@@ -0,0 +1,49 @@
+//
+//  ForestOwnerAddView.swift
+//  ForestFlow
+//
+//  Created by Roman Indermühle on 05.01.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ForestOwnerAddView: View {
+    @Query private var forestOwners: [ForestOwner]
+    @Environment(\.modelContext) var context
+    
+    @State private var showAlert = false
+    
+    @State private var name: String = ""
+    
+    var body: some View {
+        List {
+            
+        }
+        .overlay(alignment: .bottomTrailing) {
+            PlusButton()
+                .button {
+                    showAlert.toggle()
+                }
+                .alert("Hinzufügen", isPresented: $showAlert, actions: {
+                    TextField("Name", text: $name)
+                    
+                   Button("Speichern", action: {
+                       saveForestOwner()
+                   })
+                    Button("Abbrechen", role: .cancel, action: {})
+                }, message: {
+                    Text("Bitte gib den Namen des Waldbesitzers ein.")
+                })
+        }
+    }
+    
+    func saveForestOwner() {
+        let forestOwner = ForestOwner(name: name)
+        context.insert(forestOwner)
+    }
+}
+
+#Preview {
+    ForestOwnerAddView()
+}
