0d5a4126fd076bce3f42ad160d52af07bc3c11df
Roman Indermühle <roman.indermuehle@icloud.com>
2024-01-05 15:29:41 +0100

    added navigationLink to cards

--- ForestFlow/App/Settings/SettingsView.swift
+++ ForestFlow/App/Settings/SettingsView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text("Settings")
-            .font(.Bold.extraLarge)
+        NavigationStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    NavigationLink(destination: Text("Tarif")) {
+                        Card(name: "Tarif", systemImage: "dollarsign.circle.fill")
+                    }
+                    NavigationLink(destination: Text("Waldbesitzer")) {
+                        Card(name: "Waldbesitzer", systemImage: "person.text.rectangle.fill")
+                    }
+                }
+                NavigationLink(destination: Text("Baumart")) {
+                    Card(name: "Baumart", systemImage: "tree.circle.fill")
+                }
+                Spacer()
+            }
+            .padding(.top, 15)
+            .navigationTitle("Einstellungen")
+        }
     }
 }
 
