a8fd09a9b6e97f1fdfa07d089c6c0df013db96cc
simon-zwicker <github@zwicker.io>
2024-01-05 15:40:41 +0100

    fixed Card Design

--- ForestFlow/Utils/UIComponents/Card.swift
+++ ForestFlow/Utils/UIComponents/Card.swift
@@ -8,32 +8,25 @@
 import SwiftUI
 
 struct Card: View {
-    private let cardSize = (UIScreen.main.bounds.width - 60) / 2
+    private let cardSize: CGFloat = (UIScreen.main.bounds.width - 60.0) / 2.0
     var name: String
     var systemImage: String
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .frame(width: cardSize, height: 150)
-                .foregroundStyle(.white)
-                .shadow(radius: 8)
-                
-            
-            VStack {
-                Image(systemName: systemImage)
-                    .symbolRenderingMode(.hierarchical)
-                    .foregroundStyle(Color.accentColor)
-                    .font(.Bold.large)
-                    .frame(width: cardSize, alignment: .topLeading)
-                    .offset(x: 15, y: -20)
-                
-                Text(name)
-                    .font(.Bold.title4)
-                    .frame(width: cardSize, alignment: .center)
-            }
-           
+            Image(systemName: systemImage)
+                .symbolRenderingMode(.hierarchical)
+                .foregroundStyle(Color.accentColor)
+                .font(.Bold.large)
+                .position(x: 35, y: 30)
+
+            Text(name)
+                .font(.Bold.title4)
         }
+        .frame(width: cardSize, height: cardSize)
+        .background(.white)
+        .clipShape(.rect(cornerRadius: 25.0))
+        .shadow(radius: 5)
     }
 }
 
