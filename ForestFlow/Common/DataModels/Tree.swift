3cb5f5e25b2f65a5af6a37b6ef993d3032703094
Roman Indermühle <roman.indermuehle@icloud.com>
2024-01-21 18:20:39 +0100

    Added remark property

--- ForestFlow/Common/DataModels/Tree.swift
+++ ForestFlow/Common/DataModels/Tree.swift
@@ -10,17 +10,19 @@ import SwiftData
 
 @Model
 class Tree {
-    var woodType: WoodType //WoodType Enum
+    var woodType: WoodType 
     var stage: Int
     var latitude: CGFloat
     var longitude: CGFloat
     var forest: Forest?
+    var remark: Remark
 
-    init(woodType: WoodType, stage: Int, lat: CGFloat, long: CGFloat, forest: Forest? = nil) {
+    init(woodType: WoodType, stage: Int, lat: CGFloat, long: CGFloat, forest: Forest? = nil, remark: Remark) {
         self.woodType = woodType
         self.stage = stage
         self.latitude = lat
         self.longitude = long
         self.forest = forest
+        self.remark = remark
     }
 }
