6994b0db251afb40dcab5373fb1d65768f876df1
Roman Indermühle <roman.indermuehle@icloud.com>
2024-03-10 20:08:54 +0100

    ForestOwner and remark have an initial value

--- ForestFlow/App/Tree/TreeAddView.swift
+++ ForestFlow/App/Tree/TreeAddView.swift
@@ -12,24 +12,25 @@ struct TreeAddView: View {
     @Query private var remarks: [Remark]
     @Query private var woodTypes: [WoodType]
     @Query private var forestOwners: [ForestOwner]
-
+    
     @Environment(\.dismiss) var dismiss
     @Environment(\.modelContext) var context
-
+    
     @Bindable var forest: Forest
-
+    
     @State var rateValue: RateValue?
     @State var woodType: WoodType?
-    @State var stage: Int = 1
     @State var forestOwner: ForestOwner?
     @State var remark: Remark?
     @State var locationManager: LocationManager = .init()
-
+    
     var body: some View {
         VStack {
             Form {
                 Section("Bauminformationen") {
+                    
                     CircleSelection(items: woodTypes, selected: $woodType)
+                    
                     if let rateValues = forest.rateType?.rateValues.sorted(by: { $0.stage < $1.stage }) {
                         CircleSelection(items: rateValues, selected: $rateValue)
                     }
@@ -37,23 +38,25 @@ struct TreeAddView: View {
                         Text("Volumen: \(volume.string(decimalPlaces: 2))")
                     }
                     
-
+                    
                     Picker("Waldbesitzer", selection: $forestOwner) {
                         ForEach(forestOwners, id: \.id) { forestOwner in
                             Text(forestOwner.name)
+                                .tag(Optional(forestOwner))
                         }
                     }
                     
                     Picker("Bemerkung", selection: $remark) {
                         ForEach(remarks, id: \.id) { remark in
                             Text(remark.name)
+                                .tag(Optional(remark))
                         }
                     }
                 }
             }
-
+            
             Spacer()
-
+            
             Text("Hinzufügen")
                 .font(.Bold.title2)
                 .foregroundStyle(Color.accentColor)
@@ -64,12 +67,20 @@ struct TreeAddView: View {
         }
         .onAppear {
             locationManager.request()
+            
+            if forestOwner == nil {
+               forestOwner = forestOwners.first
+            }
+            
+            if remark == nil {
+                remark = remarks.first
+            }
         }
     }
-
+    
     func saveTree() {
         guard let location = locationManager.location, let woodType, let remark else { return }
-        let tree = Tree(woodType: woodType, stage: stage, lat: location.latitude, long: location.longitude, forest: forest, remark: remark)
+        let tree = Tree(woodType: woodType, rateValue: rateValue, lat: location.latitude, long: location.longitude, forest: forest, remark: remark)
         context.insert(forest)
         forest.trees.append(tree)
         dismiss()
