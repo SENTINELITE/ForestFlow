a80f142b64af449afb975db77acff01ab18464cf
Roman Indermühle <roman.indermuehle@icloud.com>
2024-03-10 20:06:33 +0100

    Added rateType for relation

--- ForestFlow/Common/DataModels/RateValue.swift
+++ ForestFlow/Common/DataModels/RateValue.swift
@@ -13,10 +13,12 @@ class RateValue: Selectable {
     var stage: Int
     var volume: Double
     var name: String
+    var rateType: RateType?
 
-    init(stage: Int, volume: Double) {
+    init(stage: Int, volume: Double, rateType: RateType? = nil) {
         self.stage = stage
         self.volume = volume
         self.name = stage.description
+        self.rateType = rateType
     }
 }
