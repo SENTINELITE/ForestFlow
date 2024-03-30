4c9b27b599c3a1839999863cfe8b6d66144142e7
Roman Indermühle <roman.indermuehle@icloud.com>
2024-03-30 23:32:55 +0100

    Disabled modifier added

--- ForestFlow/App/Settings/WoodType/WoodTypeModifyView.swift
+++ ForestFlow/App/Settings/WoodType/WoodTypeModifyView.swift
@@ -32,6 +32,7 @@ struct WoodTypeModifyView: View {
                     .background(Color.accentColor)
                     .clipShape(RoundedRectangle(cornerRadius: 15))
             }
+            .disabled(name.isEmpty)
             
         }
         .padding()
