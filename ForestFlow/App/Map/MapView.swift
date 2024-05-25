b2b268c26e44a4f67e371cecd95e50fc0ad31353
Roman Indermühle <roman.indermuehle@icloud.com>
2024-05-25 08:52:38 +0200

    MapView added

---
+++ ForestFlow/App/Map/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  ForestFlow
+//
+//  Created by Roman Indermühle on 25.05.2024.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
